a05aef09dfe874ebafc7a9661a3c990fd9a168fc
flinux@vongellhorn.ch <flinux@vongellhorn.ch>
2019-06-10 14:33:21 +0200

    - Updating license year - Adding new desktoptheme - Adding new look-and-feel - Adding Solus Wallpaper - changes to xdg 	- kdeglobals 	- plasmarc 	- kdeglobals

--- env/50-solus-defaults.sh
+++ env/50-solus-defaults.sh
@@ -1,5 +1,5 @@
 # Begin /usr/share/xdg/plasma-workspace/env/50-solus-defaults.sh
-	
+
 # Load agent if it isn't already
 if [ -z "${SSH_AGENT_PID}" ]; then
     eval `ssh-agent -s`
@@ -9,8 +9,5 @@ fi
 if [ -f /usr/bin/ksshaskpass ]; then
     export SSH_ASKPASS="/usr/bin/ksshaskpass"
 fi
-
-# Use plasma firefox file dialog
-export GTK_USE_PORTAL=1
 
 # End /usr/share/xdg/plasma-workspace/env/50-solus-defaults.sh
