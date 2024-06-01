298be0479505959967f31db644bf04ca39dd07c1
Reilly Brogan <reilly@reillybrogan.com>
2024-05-31 20:22:20 -0500

    Tag 4.6.0
    Signed-off-by: Reilly Brogan <reilly@reillybrogan.com>

--- mkrelease.sh
+++ mkrelease.sh
@@ -6,7 +6,7 @@ meson --prefix /usr build
 ninja dist -C build
 
 # Bump in tandem with meson.build, run script once new tag is up.
-VERSION="4.3"
+VERSION="4.6.0"
 TAR="plasma-desktop-branding-${VERSION}.tar.xz"
 mv build/meson-dist/$TAR .
 
